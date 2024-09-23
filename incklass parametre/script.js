aefc4fc2113fdf49e08a76cfa852fced23f308cf
Frederikke¨ <frvo0001@stud.kea.dk>
2024-09-23 08:49:16 +0200

    javascript

---
+++ incklass parametre/script.js
@@ -0,0 +1,21 @@
+// "use strict"
+// const firstName = "Frederikke";
+// const lastName = "Volkdal";
+// sayHello(lastName);
+// function sayHello(firstName){
+// console.log (`Hello ${firstName}`);
+// }
+// const name= "Frederikke";
+// const animalT= "dog";
+// const animalN= "Joey";
+// const raceT= "Staffordshire Bullterrier";
+
+// presentPet("Frederikke", "dog", "Staffordshire Bullterrier", "Joey");
+// function presentPet(name, animalT, raceT, animalN){
+// console.log (
+//     `My name is ${name}, I have a ${animalT}, who is a ${raceT} called ${animalN}`
+// );
+// }
+
+
+
